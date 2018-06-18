5e2745b26ced1242c1547a0518873254934c2dd2
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2018-06-18 12:08:20 +0100

    Update check_nodeos.sh

--- check_nodeos.sh
+++ check_nodeos.sh
@@ -30,7 +30,7 @@ function notify()
 {
     MESSAGE=$1
     echo $MESSAGE
-      [[ ! -z $VICTOROPS_WEBHOOK ]] && curl -s -X POST --data-urlencode "payload={\"message_type\": \"critical\", \"entity_id"\": \"keepalived\", \"entity_display_name\": \"$MESSAGE\"}" $VICTOROPS_WEBHOOK > /dev/null
+      [[ ! -z $VICTOROPS_WEBHOOK ]] && curl -X POST -d '{"entity_id":"keepalived","message_type":"critical","state_message":"$MESSAGE"}' $VICTOROPS_WEBHOOK > /dev/null
 }
 
 
